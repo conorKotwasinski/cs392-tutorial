f8eec08b6c072d182e5c07996d1b45aa85868245
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2023-09-22 13:52:52 -0500

    Course Selector

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -2,38 +2,50 @@ import React, { useState } from 'react';
 import TermSelector from './TermSelector';
 import './Courses.css';
 
-const CourseList = ({ courses }) => {
-  const [term, setTerm] = useState('Fall');
-  
-  const termCourses = Object.keys(courses)
-    .filter(key => courses[key].term === term)
-    .map(key => courses[key]);
+const toggle = (x, lst) => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
+);
 
-  return (
-    <>
-      <TermSelector term={term} setTerm={setTerm} />
-      <div className="course-list">
-        {termCourses.map((course, index) => <Course key={index} course={course} />)}
-      </div>
-    </>
-  );
+const CourseList = ({ courses }) => {
+ const [term, setTerm] = useState('Fall');
+ const [selected, setSelected] = useState([]);
+ const termCourses = Object.keys(courses)
+   .filter(key => courses[key].term === term)
+   .map(key => courses[key]);
+
+ return (
+   <>
+     <TermSelector term={term} setTerm={setTerm} />
+     <div className="course-list">
+       {termCourses.map((course, index) => <Course key={index} course={course} selected={selected} setSelected={ setSelected }/>)}
+     </div>
+   </>
+ );
 };
 
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-content">
-        <h4 className="card-title">
-          {course.term} CS {course.number}
-        </h4>
-        <p className="card-text">
-          {course.title}
-        </p>
-      </div>
-      <hr/>
-      <p>{course.meets}</p>
-    </div>
-  </div>
-);
+const Course = ({ course, selected, setSelected }) => {
+ const isSelected = selected.includes(course);
+ const style = {
+   backgroundColor: isSelected ? 'lightgreen' : 'white'
+ };
+ return (
+ <div className="card m-1 p-2"
+   style={style}
+   onClick={() =>  setSelected(toggle(course, selected))}>
+   <div className="card-body">
+     <div className="card-content">
+       <h4 className="card-title">
+         {course.term} CS {course.number}
+       </h4>
+       <p className="card-text">
+         {course.title}
+       </p>
+     </div>
+     <hr/>
+     <p>{course.meets}</p>
+   </div>
+ </div>
+ )
+};
 
 export default CourseList;
