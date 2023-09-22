628791d78c4fda2b10d2e93173fd6970a70ed640
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2023-09-22 13:30:22 -0500

    Term Filter

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TermSelector from './TermSelector';
 import './Courses.css';
 
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-    {Object.keys(courses).map(key => (
-      <div key={key} className="card m-1 p-2">
-        <div className="card-body">
-          <div className="card-content">
-            <h4 className="card-title">
-              {courses[key].term} CS {courses[key].number}
-            </h4>
-            <p className="card-text">
-              {courses[key].title}
-            </p>
-          </div>
-          <hr/>
-          <p>{courses[key].meets}</p>
-        </div>
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  
+  const termCourses = Object.keys(courses)
+    .filter(key => courses[key].term === term)
+    .map(key => courses[key]);
+
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+        {termCourses.map((course, index) => <Course key={index} course={course} />)}
       </div>
-    ))}
+    </>
+  );
+};
+
+const Course = ({ course }) => (
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-content">
+        <h4 className="card-title">
+          {course.term} CS {course.number}
+        </h4>
+        <p className="card-text">
+          {course.title}
+        </p>
+      </div>
+      <hr/>
+      <p>{course.meets}</p>
+    </div>
   </div>
 );
 
-
 export default CourseList;
-
---
+++ src/components/TermSelector.jsx
@@ -0,0 +1,19 @@
+const TermSelector = ({term, setTerm}) => (
+    <div className="btn-group">
+    {
+      Object.values(terms).map(value => (
+        <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+      ))
+    }
+    </div>
+  );
+const TermButton = ({term, setTerm, checked}) => (
+    <>
+    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off" onChange={() => setTerm(term)} />
+        <label className="btn btn-success m-1 p-2" htmlFor={term}>
+        { term }
+        </label>
+    </>
+);
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+export default TermSelector
