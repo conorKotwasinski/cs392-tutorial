6e171fbba0f87dc1a513e3da0ef8882482d32b6c
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2023-09-22 03:02:24 -0500

    Fetch Data

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,19 @@ import React from 'react';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
 import 'bootstrap/dist/css/bootstrap.min.css';
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+import {useJsonQuery} from './utilities/fetch';
+const App = () => {
+  const [schedule, isLoading, error]  = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
 
-const App = () => (
+  if (isLoading) return <h1>Loading schedule...</h1>;
+
+  return (
     <div className="container">
-      <Banner title={ schedule.title } />
-      <CourseList courses={ schedule.courses } />
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
     </div>
-);
+  );
+};
 
 export default App;
 
