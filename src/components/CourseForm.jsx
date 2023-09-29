9efc4fa6bfc91e4b1618ebd60e385a4e06a52af1
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2023-09-29 18:51:45 -0500

    Add Database

--- src/components/CourseForm.jsx
+++ src/components/CourseForm.jsx
@@ -1,70 +1,129 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import { useFormData } from '../utilities/useFormData'; // Import the custom hook
+import { useFormData } from '../utilities/useFormData'; 
+import { useDbUpdate } from '../utilities/firebase';
 
-const CourseForm = ({ courses }) => {
-  const { courseId } = useParams();
+const ButtonBar = ({message, disabled}) => {
   const navigate = useNavigate();
-
-  const decodeCourseId = (id) => {
-    const parts = id.split('-');
-    const title = `${parts[0]}-${parts[1]}`;
-    const meets = parts.slice(2).join(' ').replace(/-/g, ' ');
-    return { title, meets };
-  };
-
-  const { title: initialTitle, meets: initialMeets } = decodeCourseId(courseId);
-
-  const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
-
-  const validator = (id, value) => {
-    if (id === 'title' && value.length < 2) {
-      return "Title must be at least two characters.";
-    }
-    if (id === 'meets' && value !== '' && !meetsPat.test(value)) {
-      return "Must contain days and start-end, e.g., MWF 12:00-13:20.";
-    }
-    return '';
-  };
-
-  const [formData, handleChange] = useFormData(validator, { title: initialTitle, meets: initialMeets });
-
-  const cancelForm = () => {
-    navigate('/');
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // TODO: Handle form submission
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="title" className="form-label">Course Title</label>
-        <input
-          type="text"
-          className="form-control"
-          id="title"
-          value={formData.values.title || ''}
-          onChange={handleChange}
-        />
-        {formData.errors && formData.errors.title && <div className="text-danger">{formData.errors.title}</div>}
-      </div>
-      <div className="mb-3">
-        <label htmlFor="meets" className="form-label">Meeting Times</label>
-        <input
-          type="text"
-          className="form-control"
-          id="meets"
-          value={formData.values.meets || ''}
-          onChange={handleChange}
-        />
-        {formData.errors && formData.errors.meets && <div className="text-danger">{formData.errors.meets}</div>}
-      </div>
-      <button type="button" className="btn btn-secondary" onClick={cancelForm}>Cancel</button>
-      <button type="button" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
-    </form>
+    <div className="d-flex">
+      <button type="button" className="btn btn-outline-dark me-2" onClick={() => navigate(-1)}>Cancel</button>
+      <button type="submit" className="btn btn-primary me-auto" disabled={disabled}>Submit</button>
+      <span className="p-2">{message}</span>
+    </div>
   );
 };
 
+const undoGenerateCourseId = (courseId) => {
+  // Split the courseId string at '-' to get parts
+  const parts = courseId.split('-');
+
+  // Validate if the parts contain at least the term, number, and one part of meets
+  if (parts.length < 3) {
+      throw new Error('Invalid courseId format');
+  }
+
+  // The term will always be the first part
+  const term = parts[0];
+
+  let meets = "";
+  let number = "";
+
+  // Check if the third part is a number
+  if (!isNaN(parts[2])) {
+    number = parts[1] + "-" + parts[2]
+    meets = parts.slice(3).join(' ');
+  } else {
+    // The meets is everything after the term and number
+    // So we need to rejoin parts from index 2 onwards with a space
+    number = parts[1]
+    meets = parts.slice(2).join(' ');
+  }
+
+  return {
+      term: term,
+      number: number,
+      meets: meets
+  };
+}
+
+const calculateDbCourseId = (courseId) => {
+  // Split the courseId string at '-' to get parts
+  const parts = courseId.split('-');
+
+  // Validate if the parts contain at least the term, number, and one part of meets
+  if (parts.length < 3) {
+    throw new Error('Invalid courseId format');
+  }
+
+  // The term will always be the first part
+  const term = parts[0];
+
+  // The number will always be the second part
+  const number = parts[1];
+
+  // Db format is the first char of the term plus the number
+  return term[0] + number;
+}
+
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const validator = (id, value) => {
+  if (id === 'title' && value.length < 2) {
+    return "Title must be at least two characters.";
+  }
+  if (id === 'meets' && value !== '' && !meetsPat.test(value)) {
+    return "Must contain days and start-end, e.g., MWF 12:00-13:20.";
+  }
+  return '';
+};
+
+const InputField = ({name, text, state, change}) => (
+  <div className="mb-3">
+    <label htmlFor={name} className="form-label">{text}</label>
+    <input className="form-control" id={name} name={name} 
+      defaultValue={state.values?.[name]} onChange={change} />
+    <div className="invalid-feedback">{state.errors?.[name]}</div>
+  </div>
+);
+
+const CourseForm = ({ courses }) => {
+  const { courseId } = useParams();
+  const dbCourseId = calculateDbCourseId(courseId);
+  
+  let courseDetails = undoGenerateCourseId(courseId);
+  
+  let matchedCourse = null;
+  for (let c of Object.values(courses)) {
+    if (c.term === courseDetails.term && c.number === courseDetails.number) {
+      matchedCourse = c;
+      break;
+    }
+  }
+
+  // Use matchedCourse in the subsequent code, but check if it's non-null first
+  if (!matchedCourse) {
+    // Handle the case when the course wasn't found in the courses object
+    // This might be returning a default object, showing an error, etc.
+    return <div>Course not found</div>;  // Example handling
+  }
+
+  const [update, result] = useDbUpdate(`/courses/${dbCourseId}`);
+
+  const [state, change] = useFormData(validator, { title: matchedCourse.title, meets: matchedCourse.meets });
+  const submit = (evt) => {
+    evt.preventDefault();
+    if (!state.errors) {
+      update(state.values);
+    }
+  };
+
+  return (
+    <form onSubmit={submit} noValidate className={state.errors ? 'was-validated' : null}>
+      <InputField name="title" text="Course Title" state={state} change={change} />
+      <InputField name="meets" text="Meeting Times" state={state} change={change} />
+      <ButtonBar message={result?.message} disabled={!!state.errors} />
+    </form>
+  );
+};
 export default CourseForm;
